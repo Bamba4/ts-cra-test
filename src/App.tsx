3f00dc5ed2bcca8c24d86eabd67e4345193175e9
Bamba <bamba4>
2022-05-10 11:32:04 +0000

    My second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import pokemon from './pokemon.json'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PokemonInterface {
+  id: number
+  name: Name
+  type: string[]
+  base: Base
 }
 
-export default App;
+interface Base {
+  HP: number
+  Attack: number
+  Defense: number
+  'Sp. Attack': number
+  'Sp. Defense': number
+  Speed: number
+}
+
+interface Name {
+  english: string
+  japanese: string
+  chinese: string
+  french: string
+}
+
+const PokemonRow = ({
+  pokemonProps,
+  onSelect,
+}: {
+  pokemonProps: PokemonInterface
+  onSelect: { (p: PokemonInterface): void }
+}) => {
+  return (
+    <tr>
+      <td>{pokemonProps.name.english}</td>
+      <td>{pokemonProps.type.join(', ')}</td>
+      <td>
+        <button onClick={() => onSelect(pokemonProps)}>Sellect!</button>
+      </td>
+    </tr>
+  )
+}
+
+function App() {
+  const [filter, setFilter] = React.useState('')
+  const [selectItem, setSelectItem] = React.useState<PokemonInterface | null>(null)
+  return (
+    <div
+      style={{
+        margin: 'auto',
+        width: 800,
+        paddingTop: '1rem',
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: '70% 80%', gridColumnGap: '1rem' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: '70% 30%', gridColumnGap: '1rem' }}>
+          <div>
+            <input type="text" value={filter} onChange={(evt) => setFilter(evt.target.value)} />
+            <table width="100%">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pokemon
+                  .filter((p: PokemonInterface) =>
+                    p.name.english.toLowerCase().includes(filter.toLowerCase())
+                  )
+                  .slice(0, 20)
+                  .map((p: PokemonInterface) => (
+                    <PokemonRow
+                      key={p.id}
+                      pokemonProps={p}
+                      onSelect={(pokemonValue: PokemonInterface) => setSelectItem(pokemonValue)}
+                    />
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        {selectItem && (
+          <div>
+            <h1>{selectItem.name.english}</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
